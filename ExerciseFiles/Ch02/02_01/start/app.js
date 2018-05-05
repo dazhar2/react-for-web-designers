6741ef394d26f85efd79fa9193c532c4c921a073
Dania <dazhar2@uic.edu>
2018-05-05 03:49:37 +0000

    event handling size part 9

--- ExerciseFiles/Ch02/02_01/start/app.js
+++ ExerciseFiles/Ch02/02_01/start/app.js
@@ -13,11 +13,16 @@
         );
       });
     }
+    
+     function onSizeChange(evt) {
+      props.handleSizeChange(evt.target.value);
+    }
+
 
     return (
       <div className="field-group">
         <label htmlFor="size-options">Size:</label>
-        <select defaultValue={props.size} name="sizeOptions" id="size-options">
+        <select defaultValue={props.size} name="sizeOptions" id="size-options" onChange={onSizeChange}>
           {sizeOptions()}
         </select>
       </div>
@@ -61,6 +66,14 @@
         sizes: sizes
       };
     },
+    
+    handleSizeChange: function(selectedSize) {
+      var availableColors = window.Inventory.bySize[selectedSize];
+
+      this.setState({
+        colors: availableColors
+      });
+    },
 
     render: function() {
       return (
@@ -69,7 +82,8 @@
             <ProductImage color={this.state.color} />
           </div>
           <div className="selectors">
-            <SizeSelector size={this.state.size} sizes={this.state.sizes} />
+            <SizeSelector size={this.state.size} sizes={this.state.sizes} 
+                handleSizeChange={this.handleSizeChange} />
             <ColorSelector color={this.state.color} colors={this.state.colors} />
           </div>
         </div>
